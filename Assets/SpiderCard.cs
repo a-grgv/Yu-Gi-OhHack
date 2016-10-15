2fafc9c0a2a75323c40d9b81d0431abef8c1b2cc
Reni M <renim@glaroam2-157-6.wireless.gla.ac.uk>
2016-10-15 07:34:50 +0100

    added assets and effects + ui button

---
+++ Assets/SpiderCard.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SpiderCard : MonoBehaviour {
+
+	public Button attackButton;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
