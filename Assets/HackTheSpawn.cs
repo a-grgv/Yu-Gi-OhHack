17b5deab58f2ddc6e05056207d4e1d186f90d33e
Reni M <renim@glaroam2-157-6.wireless.gla.ac.uk>
2016-10-15 14:49:00 +0100

    Prefabs for each image target, add them to the hack spawner

---
+++ Assets/HackTheSpawn.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HackTheSpawn : MonoBehaviour {
+
+	public GameObject[] stuff;
+
+	// Use this for initialization
+	void Start () {
+		// So hackity hack hack... I hate myself for writing this
+		foreach (var t in stuff) {
+			Instantiate(t, transform);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
